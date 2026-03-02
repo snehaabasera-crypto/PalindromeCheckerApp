64a9a177f9c8f16c2aca7ae4187d7ef5e350480b
snehaabasera-crypto <snehaabasera@gmail.com>
2026-03-02 09:08:44 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,22 @@
 public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("=======================================");
+        System.out.println("      WELCOME TO PALINDROME CHECKER    ");
+        System.out.println("=======================================");
+
+
+
+        System.out.println("Application Name : PalindromeChecker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("---------------------------------------");
+
+
+        System.out.println("Application started successfully.");
+        System.out.println("Ready to proceed to palindrome validation...");
+    }
+
 }
