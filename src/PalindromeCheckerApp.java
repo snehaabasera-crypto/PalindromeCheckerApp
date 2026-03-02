49493647e592fb55cbb4bb73ab6dcdf37f6ada5f
snehaabasera-crypto <snehaabasera@gmail.com>
2026-03-02 09:30:43 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -17,6 +17,27 @@ public class PalindromeCheckerApp {
 
         System.out.println("Application started successfully.");
         System.out.println("Ready to proceed to palindrome validation...");
+
+        String word = "madam";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+
+        // Program ends
+        System.out.println("Program executed successfully.");
     }
 
 }
+
